6673684c57b709b0f48bf22a23c0bef97ede2f4c
Alexandr <AlMi@Mac-mini-Alexandr.local>
2018-04-02 15:37:44 +0300

    add show_password

---
+++ src/_scripts/main.js
@@ -0,0 +1,68 @@
+function animationEye( eye_frames, button ) {
+
+  button.dataset.in_process = 'true';
+
+  let _eye_frames = [];
+
+  let state = button.dataset.state;
+
+
+  if ( state === 'open' ) {
+    button.dataset.state = 'close';
+    _eye_frames = [...eye_frames];
+  } else if ( state === 'close' ) {
+    button.dataset.state = 'open';
+    _eye_frames = [...eye_frames].reverse();
+  } else {
+    return
+  }
+
+  let length = _eye_frames.length,
+    i = 0;
+
+  const openEyeInterval = setInterval(() => {
+    if ( i <  length) {
+      if ( i === 0 ) {
+        _eye_frames[i].style.display = 'block';
+      } else {
+        _eye_frames[i - 1].style.display = 'none';
+        _eye_frames[i].style.display = 'block';
+      }
+      i++;
+    } else {
+      button.dataset.in_process = 'false';
+      clearInterval(openEyeInterval);
+    }
+  }, 30);
+};
+
+window.addEventListener('load', () => {
+  const eye_button = document.querySelector('.password__icon-container-eye');
+  const bg_after = document.querySelector('.password__bg-after');
+  const lock_icon = document.querySelector('.password__icon-lock');
+  const eye_frames = document.querySelector('#eye').children;
+  const password_input = document.querySelector('.password__input');
+
+
+  eye_button.addEventListener('click', (e) => {
+    if ( e.currentTarget.dataset.in_process === 'false' ) {
+      bg_after.classList.toggle('active');
+      if ( e.currentTarget.dataset.state === 'close' ) {
+        setTimeout(() => {
+          password_input.type = 'text';
+        }, 200);
+        setTimeout(() => {
+          lock_icon.classList.toggle('active');
+        }, 300);
+      } else {
+        setTimeout(() => {
+          lock_icon.classList.toggle('active');
+          password_input.type = 'password'
+        }, 50);
+      }
+
+      animationEye( eye_frames, eye_button );
+    };
+  })
+
+});
